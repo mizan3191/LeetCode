5b68458981ec3defa84b1cec4eb52640102798da
mizanur rahaman <mizanurrahaman020195@gmail.com>
2022-08-20 09:09:36 +0600

    save

---
+++ LeetCode/1470_Shuffle_the_Array/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _1470_Shuffle_the_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] result = new int[] { 2, 5, 1, 3, 4, 7 };
+            Shuffle(result, 3);
+        }
+
+        public static int[] Shuffle(int[] nums, int n)
+        {
+            var lnt = nums.Length;
+            int[] result = new int[lnt];
+
+            var count=0;
+
+            for (var i =0; i< lnt/2; i++)
+            {
+                result[i+count] = nums[i];
+                result[i +count+ 1] = nums[lnt / 2 + i];
+                count++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/1929_Concatenation_of_Array/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _1929_Concatenation_of_Array
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 2, 1 };
+            Program.GetConcatenation(nums);
+        }
+
+        public static int[] GetConcatenation(int[] nums)
+        {
+            int[] result = new int[nums.Length * 2];
+
+            for(var i = 0; i<nums.Length; i++)
+            {
+                result[i] = nums[i];
+                result[i + nums.Length] = nums[i];
+            }
+
+            return result;
+
+        }
+    }
+}
