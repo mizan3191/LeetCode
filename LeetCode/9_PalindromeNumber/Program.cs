bb7fbf7fe9968a23ea71afbab56cab71b1638901
mizanur rahaman <mizanurrahaman020195@gmail.com>
2022-08-20 07:44:20 +0600

    save

---
+++ LeetCode/9_PalindromeNumber/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _9_PalindromeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 1215;
+
+            int newNumber = 0, remain = 0, divide = 0;
+
+            while (x > 0)
+            {
+                
+                remain = x % 10;
+                x = x / 10;
+
+                newNumber = newNumber * 10 + remain;
+            }
+
+            Console.WriteLine(newNumber);
+        }
+
+        public static bool isPalindrome(int x)
+        {
+            var copy = x;
+
+            long newNumber = 0, remain = 0;
+
+            while (x>0)
+            {
+                remain = x / 10;
+                x = x % 10;
+                newNumber = newNumber * 10 + remain;
+            }
+
+            if(copy == newNumber)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
