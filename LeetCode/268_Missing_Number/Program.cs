cc506fb827287b749c4535922b93ae1293cee821
mizanur rahaman <mizanurrahaman020195@gmail.com>
2022-08-23 07:30:38 +0600

    save

---
+++ LeetCode/268_Missing_Number/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _268_Missing_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 0, 1 };
+            Console.WriteLine(MissingNumber(nums));
+        }
+
+        public static int MissingNumber(int[] nums)
+        {
+
+            var Result = 0;
+            var n = nums.Length;
+            
+
+            for(var i = 0; i<n ; i++)
+            {
+                var missingNuber = 0;
+
+                for (var j = 0; j< n; j++)
+                {
+                    if(nums[j] == Result)
+                    {
+                        break;
+                    }
+                    if(nums[j] != Result)
+                    {
+                        missingNuber++;
+                    }
+                }
+
+                if(missingNuber == n)
+                {
+                    return Result;
+                }
+                Result++;
+            }
+
+            return Result;
+        }
+
+    }
+}
---
+++ LeetCode/414_Third_Maximum_Numbe/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _414_Third_Maximum_Numbe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 0, 1 };
+            Console.WriteLine(ThirdMax(nums));
+        }
+
+        public static int ThirdMax(int[] nums)
+        {
+            var max1 = 0;
+
+            for(var i = 0; i<nums.Length; i++)
+            {
+                if(nums[i] > max1)
+                {
+                    max1 = nums[i];
+                }
+            }
+
+            var max2 = 0;
+            for(var i = 0; i<nums.Length; i++)
+            {
+                if(nums[i] > max2 )
+                {
+                    max1 = nums[i];
+                }
+            }
+
+            return 0;
+        }
+    }
+}
