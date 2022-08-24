cd620d5e94068d7d5e17a650a9843b59925a8e26
mizanur rahaman <mizanurrahaman020195@gmail.com>
2022-08-24 07:40:47 +0600

    save

---
+++ LeetCode/344_Reverse_String/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _344_Reverse_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] s = { 'a', 'b', 'c', 'd', 'e', 'f' };
+            ReverseString(s);
+        }
+        public static void ReverseString(char[] s)
+        {
+            for(int i = s.Length-1, j=0 ; i >=s.Length/2 ; i--, j++)
+            {
+                var x = s[i];
+                s[i] = s[j];
+                s[j] = x;
+            }
+        }
+    }
+}
---
+++ LeetCode/387_First_Unique_Character_in_a_String/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _387_First_Unique_Character_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "aabb";
+           
+            Console.WriteLine(FirstUniqChar(s));
+        }
+        public static int FirstUniqChar(string s)
+        {
+            var x = -1;
+            var c = s.ToCharArray();
+            for(var i=0; i<c.Length; i++)
+            {
+                var m = 0;
+                for(var j=0; j<c.Length; j++)
+                {
+                    if (c[i] == c[j])
+                    {
+                        m++;
+                    }
+                    if(m >= 2)
+                    {
+                        break;
+                    }
+                }
+
+                if(m == 1)
+                {
+                    return i;
+                }
+            }
+
+            return x;
+        }
+    }
+}
